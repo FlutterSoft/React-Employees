564b580b0db40a059a136e9bdb1dfd7867b1b6ab
Sam Brown <jumpbro@hotmail.com>
2022-08-21 12:25:25 +1000

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
-
+import JSONDATA from './MOCK_DATA.json'
 function App() {
+  
+  const [search, setSearch] = useState('')
+
+  const employeesList = JSONDATA.filter(employee => employee.first_name.toLowerCase().includes(search.toLowerCase())).map( employee => {
+    return(
+      <li key={employee.id}> {employee.first_name}</li>
+    )
+  })
+
+  function handleSearch(e){
+    setSearch(e.target.value)
+  }
+
+  function Table(){
+    const rows = JSONDATA.filter(employee => {
+      return (
+        employee.first_name
+          .toLowerCase().
+          includes(search.toLowerCase())
+          ||
+          employee.last_name
+          .toLowerCase().
+          includes(search.toLowerCase())         
+          ||
+          employee.email
+          .toLowerCase().
+          includes(search.toLowerCase())         
+          ||
+          employee.job_title
+          .toLowerCase().
+          includes(search.toLowerCase())         
+      )
+    }).map(employee => {
+        return(
+          <tr key={employee.id}>
+            <td>{employee.first_name}</td>
+            <td>{employee.last_name}</td>
+            <td>{employee.email}</td>
+            <td>{employee.job_title}</td>
+          </tr>
+        )
+    })
+    return(
+      <table>
+        <tbody>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Job Title</th>
+          </tr>
+          {rows}
+        </tbody>
+      </table>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employees</h1>
+      <input value={search} placeholder='Search Employee' onChange={handleSearch}></input>
+
+      <Table />
+      
     </div>
   );
 }
